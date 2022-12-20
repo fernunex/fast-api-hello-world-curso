61f9cac92c632212477d9a244b2b36ec61c354aa
fernandonunex <fer.bsof@outlook.com>
2022-12-19 22:48:50 -0600

    validations_body

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ app = FastAPI()
 
 # Models
 
+class Location(BaseModel):
+    city: str
+    state: str
+    country: str
+
+
 class Person(BaseModel):
     first_name: str
     last_name: str
@@ -61,3 +67,21 @@ def show_person(
         )
 ):
     return {person_id: "Exist!"}
+
+# Validaciones: Request Body
+
+@app.put("/person/{person_id}")
+def update_person(
+    person_id: int = Path(
+        ...,
+        title="Person ID",
+        description="This is the person ID",
+        gt=0
+    ),
+    person: Person = Body(...),
+    location: Location = Body(...)
+):
+    result = person.dict()
+    result.update(location.dict())
+
+    return result
