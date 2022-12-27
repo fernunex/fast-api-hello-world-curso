7192cbe4f56217dafa53407ab5d39fe7884a01b6
fernandonunex <fer.bsof@outlook.com>
2022-12-26 22:39:12 -0600

    summary_and_description

--- main.py
+++ main.py
@@ -117,9 +117,21 @@ def home():
     path="/person/new",
     response_model=PersonOut,
     status_code=status.HTTP_201_CREATED,
-    tags=["Persons"]
+    tags=["Persons"],
+    summary="Create Person in the app"
     )
 def create_person(person: Person = Body(...)): # "..."" means that the parameter is obligatory
+    """
+    Create Person
+
+    This path operation creates a person in the app and save the information in the database
+
+    Parameters:
+    - Request body parameter:
+        - **person: Person** -> A person model with first name, last name, age, hair color and marital status 
+
+    Returns a person model with first name, last name, age, hari color and marital status
+    """
     return person
 
 # Validaciones: Query Parameters
